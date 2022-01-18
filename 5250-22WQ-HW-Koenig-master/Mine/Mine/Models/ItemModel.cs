68c1c8c22ddb32568ced89aedb2553090c1eeaee
Nik Stires <nik.stires@gmail.com>
2022-01-17 16:52:46 -0800

    Added Comments to ItemModel.cs

--- 5250-22WQ-HW-Koenig-master/Mine/Mine/Models/ItemModel.cs
+++ 5250-22WQ-HW-Koenig-master/Mine/Mine/Models/ItemModel.cs
@@ -2,10 +2,16 @@
 
 namespace Mine.Models
 {
+    /// <summary>
+    /// Items for characters and monsters to use
+    /// </summary>
     public class ItemModel
     {
+        //The id for the item
         public string Id { get; set; }
+        //The display text for the item
         public string Text { get; set; }
+        //The description for the item
         public string Description { get; set; }
 
         //The value of the item +9 damage
