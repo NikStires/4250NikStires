38bd7b117d51805eb70827271d448c94034ccc44
Nik Stires <nik.stires@gmail.com>
2022-01-31 18:34:22 -0800

    Added TaskExtension to shared project, TaskExtensions.cs

---
+++ 5250-22WQ-HW-Koenig-master/Mine/Mine/TaskExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mine
+{
+    public static class TaskExtensions
+    {
+        // NOTE: Async void is intentional here. This is provide a way to call all 
+        //async method from constructor
+        // while communicating intent to fire and forget, amd allow handling of 
+        //exception
+        public static async void SafeFireAndForget(this Task task, bool
+            returntoCallingContext, Action<Exception> onException = null)
+        {
+            try
+            {
+                await task.ConfigureAwait(returntoCallingContext);
+            }
+            // If the provided action is not null, catch and pass the thrown 
+            //excecption
+            catch (Exception ex) when (onException != null)
+            {
+                onException(ex);
+            }
+        }
+    }
+}
