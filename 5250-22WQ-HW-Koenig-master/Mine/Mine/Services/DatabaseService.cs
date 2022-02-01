e1337a6573a7863949c8d85c0597558b505f69ac
Nik Stires <nik.stires@gmail.com>
2022-01-31 19:36:44 -0800

    Partial Checkin – Added UpdateAsync, need to add ID value to DatabaseService.cs

--- 5250-22WQ-HW-Koenig-master/Mine/Mine/Services/DatabaseService.cs
+++ 5250-22WQ-HW-Koenig-master/Mine/Mine/Services/DatabaseService.cs
@@ -59,9 +59,25 @@ namespace Mine.Services
             return true;
         }
 
-        public Task<bool> UpdateAsync(ItemModel item)
+        /// <summary>
+        /// updates item in database returns true on success
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateAsync(ItemModel item)
         {
-            throw new NotImplementedException();
+            if(item == null)
+            {
+                return false;
+            }
+
+            var result = await Database.UpdateAsync(item);
+            if(result == 0)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public Task<bool> DeleteAsync(string id)
